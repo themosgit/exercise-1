3758aad90cf79c52933ba523491c3af96814fc90
marcusstefan <120809871+marcusstefan@users.noreply.github.com>
2023-11-03 09:17:53 +0200

    Add files via upload

--- Exercise1/examples/hp_main.c
+++ Exercise1/examples/hp_main.c
@@ -5,7 +5,14 @@
 #include "bf.h"
 #include "hp_file.h"
 
+//#define RECORDS_NUM 1 // you can change it if you want
+//#define RECORDS_NUM 10 // you can change it if you want
+//#define RECORDS_NUM 100 // you can change it if you want
 #define RECORDS_NUM 1000 // you can change it if you want
+//#define RECORDS_NUM 10000 // you can change it if you want
+//#define RECORDS_NUM 100000 // you can change it if you want
+//#define RECORDS_NUM 1000000 // you can change it if you want
+//#define RECORDS_NUM 10000000 // you can change it if you want
 #define FILE_NAME "data.db"
 
 #define CALL_OR_DIE(call)     \
@@ -17,28 +24,35 @@
     }                         \
   }
 
+
 int main() {
-  BF_Init(LRU);
 
-  HP_CreateFile(FILE_NAME);
-  int file_desc;
-/*
-  HP_info* hp_info2=HP_OpenFile(FILE_NAME, &file_desc);
-  
-  Record record;
-  srand(12569874);
-  int r;
-  printf("Insert Entries\n");
-  for (int id = 0; id < RECORDS_NUM; ++id) {
-    record = randomRecord();
-    HP_InsertEntry(file_desc,hp_info2, record);
-  }
+    remove(FILE_NAME);
 
-  printf("RUN PrintAllEntries\n");
-  int id = rand() % RECORDS_NUM;
-  printf("\nSearching for: %d",id);
-  HP_GetAllEntries(file_desc,hp_info2, id);
+    BF_Init(LRU);
+    HP_CreateFile(FILE_NAME);
+    int file_desc;
 
-  HP_CloseFile(file_desc,hp_info2);
-  BF_Close();*/
+    HP_info *hp_info = HP_OpenFile(FILE_NAME, &file_desc);
+
+
+    Record record;
+    srand(12569874);
+    printf("Insert Entries\n");
+    for (int id = 0; id < RECORDS_NUM; ++id) {
+        record = randomRecord();
+        HP_InsertEntry(file_desc, hp_info, record);
+    }
+
+    printf("RUN PrintAllEntries\n");
+    int id = rand() % RECORDS_NUM;
+
+    printf("Searching for: %d\n", id);
+    HP_GetAllEntries(file_desc, hp_info, id);
+
+    HP_CloseFile(file_desc, hp_info);
+    BF_Close();
+
 }
+
+
